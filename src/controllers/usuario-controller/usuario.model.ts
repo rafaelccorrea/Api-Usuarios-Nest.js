2664eac341fcc071a7e74f09359cfc7409335884
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2021-12-07 14:10:33 -0300

    Validacao

--- src/controllers/usuario-controller/usuario.model.ts
+++ src/controllers/usuario-controller/usuario.model.ts
@@ -1,7 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class Usuario {
     id: number;
+
+    @IsNotEmpty({
+        message: 'nome e obrigatorio'
+    })
+    @IsString()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty({
+        message: 'Senha obrigatoria'
+    })
     password: string;
     dataInitial: Date;
 }
