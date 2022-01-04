d408abb563639027bd8ad1ade981d90326f3f185
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-01-04 08:58:07 -0300

    Configuração tratamento de erros

--- src/controllers/app.module.ts
+++ src/controllers/app.module.ts
@@ -1,11 +1,16 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { FiltroExcessao } from './usuario/filtros-exececao';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [UsuarioModule],
   controllers: [],
   providers: [
+    {
+      provide: APP_FILTER,
+      useClass: FiltroExcessao
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor
--- src/controllers/usuario/filtros-exececao.ts
+++ src/controllers/usuario/filtros-exececao.ts
@@ -1,12 +1,33 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { AbstractHttpAdapter, HttpAdapterHost } from "@nestjs/core";
 
 @Catch()
 
 export class FiltroExcessao implements ExceptionFilter{
+
+    private httpAdapter: AbstractHttpAdapter;
+
+    construtor(adapterHost: HttpAdapterHost) {
+        this.httpAdapter = adapterHost.httpAdapter;
+    }
+
     catch(exception: Error, host: ArgumentsHost) {
         const contexto = host.switchToHttp();
         const requisicao = contexto.getRequest();
         const resposta = contexto.getResponse();
-    }
 
+        const { status, body } = exception instanceof HttpException ? {
+            status: exception.getStatus(),
+            body: exception.getResponse()
+        } : {
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+            body: {
+                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                timestamp: new Date().toISOString(),
+                path: requisicao.path
+            }
+        };
+
+        this.httpAdapter.reply(resposta, body, status);
+    }
 }
